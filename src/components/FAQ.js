0041d9145afa0d51db493f3717d89340d130dad4
Gerald Nwanze <nwanzegerald@gmail.com>
2023-08-16 13:11:07 +0100

    modifying the mobile view

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,38 +1,84 @@
 function FAQ() {
-
     return (
         <section className="mt-64">
             <h2 className="text-[#85B5F8] text-[36px] font-ibm text-center mb-5 max-[800px]:text-[30px]">Frequently asked questions</h2>
             <p className="text-[#CCCCCC] text-center text-[20px] max-[800px]:text-[18px] max-[800px]:px-5 font-source">Everything you need to know about  Chaincrib .</p>
 
-            <div className="grid grid-cols-2">
-            <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" checked="checked" /> 
-            <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-            </div>
-            <div className="collapse-content"> 
-                <p>hello</p>
-            </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" /> 
-            <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-            </div>
-            <div className="collapse-content"> 
-                <p>hello</p>
-            </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" /> 
-            <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-            </div>
-            <div className="collapse-content"> 
-                <p>hello</p>
-            </div>
-            </div>
+            <div className="">
+                <div className="w-3/4 mx-auto">
+                <div className="w-full flex items-center justify-center h-screen ">
+                    <div className="w-full px-8 mx-auto mt-2 space-y-1 ">
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">What is ChainCrib?</summary>
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#CCCCCC" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                ChainCrib is a pioneering web3 real estate platform allowing users to invest in high-return rental properties without full ownership.
+                                </p>
+                            </div>
+                        </details>
+
+                        <hr className="hr" />
+
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">How do I earn from owning a property fraction?</summary>
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                Own a property fraction, and you're set to earn rental yields. Once occupied, rental income is distributed monthly to your Cardano wallet.
+                                </p>
+                            </div>
+                        </details>
+
+                        <hr className="hr" />
+
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">How do we determine property prices?</summary>
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                We base our prices on real-time market appraisals, factoring in insurance, documentation, and furnishings.
+                                </p>
+                            </div>
+                        </details>
+
+                        <hr className="hr" />
+
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">Can anyone globally invest in ChainCrib?</summary>
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                Absolutely! If you have a Cardano wallet, you can buy ChainCrib property fractions using ADA.
+                                </p>
+                            </div>
+                        </details>
+
+                        <hr className="hr" />
+
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">Am I crowdfunding or truly owning a property fraction with the NFTs?</summary>
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                With ChainCrib, you genuinely own a fraction of the property through the NFT you purchase.
+                                </p>
+                            </div>
+                        </details>
+
+                        <hr className="hr" />
+
+                        <details className="rounded-lg py-5">
+                            <summary className="px-4 py-2 text-[18px] cursor-pointer text-[#FFFFFF] font-source font-[600]">Who handles property maintenance?</summary>
+                            <div className="">
+                                <p className="leading-6 px-3 py-2 text-[#CCCCCC] font-source text-[16px] font-[400]">
+                                We've partnered with top-tier property management companies. They maintain properties and provide regular detailed reports.
+                                </p>
+                            </div>
+                        </details>
+                    </div>
+                </div>
+                </div>
             </div>
 
         </section>
