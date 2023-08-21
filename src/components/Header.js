9f02ee319c4d1a1cc073af4707e511fecd33ed93
Gerald Nwanze <nwanzegerald@gmail.com>
2023-08-21 10:27:02 +0100

    made changes to the team section

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,23 +16,23 @@ function toggleMenu() {
 
 function Header() {
     return (
-        <div className="w-full h-[100px] bg-navy-blue flex items-center justify-around max-[800px]:justify-between max-[800px]:px-6 mb-10 backdrop-blur-xl pt-10 max-[800px]:pt-0">
+        <div className="w-full bg-navy-blue flex items-center justify-around max-[800px]:justify-between max-[800px]:px-6 mb-10 backdrop-blur-xl pt-10 max-[800px]:pt-0">
             <div>
                 <a href="/" className='flex gap-5 items-center'>
-                    <img src={logo} alt="logo" className='w-12 h-12' />
-                    <h1 className='logo text-[40px] font-[900] max-[800px]:text-[24px]'>Chaincrib</h1>
+                    <img src={logo} alt="logo" className='w-10 h-10' />
+                    <h1 className='logo text-[20px] font-[900] max-[800px]:text-[24px]'>Chaincrib</h1>
                 </a>
             </div>
             <div id='menu' className="text-white flex gap-x-8 max-[800px]:hidden">
-                <a href="/" target='_blank' className="px-[28px] py-[12px] h-12 border rounded-full border-[#85B5F8]">About</a>
-                <a href="/" target='_blank' className="px-[28px] py-[12px] h-12 border rounded-full border-[#85B5F8]">How It Works</a>
-                <a href="/" target='_blank' className="px-[28px] py-[12px] h-12 border rounded-full border-[#85B5F8]">Team</a>
-                <a href="/" target='_blank' className="px-[28px] py-[12px] h-12 border rounded-full border-[#85B5F8]">FAQ</a>
+                <a href="/" target='_blank' className="px-[24px] py-[8px] h-10 border rounded-full border-[#85B5F8]">About</a>
+                <a href="/" target='_blank' className="px-[24px] py-[8px] h-10 border rounded-full border-[#85B5F8]">How It Works</a>
+                <a href="/" target='_blank' className="px-[24px] py-[8px] h-10 border rounded-full border-[#85B5F8]">Team</a>
+                <a href="/" target='_blank' className="px-[24px] py-[8px] h-10 border rounded-full border-[#85B5F8]">FAQ</a>
             </div>
             <div className='max-[800px]:hidden'>
-                <a href="/" className="px-[50px] py-[12px] rounded-full text-white bg-[#1656B9] border border-[#2074F5] flex gap-x-2 items-center">
-                    <span className='text-xl'>Join Discord</span>
-                    <img src={discord} alt="discord" className='w-7 h-6' />
+                <a href="/" className="px-[18px] py-[8px] rounded-full text-white bg-[#1656B9] border border-[#2074F5] flex gap-x-2 items-center">
+                    <span className='text-lg'>Join Discord</span>
+                    <img src={discord} alt="discord" className='w-6 h-5' />
                 </a>
             </div>
             <div className='hidden max-[800px]:block'>
